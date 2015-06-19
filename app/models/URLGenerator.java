15c14f3547c6c661193a60988eec7c8d7cd2e4ba
Apurv Sethi <Apurv@Apurvs-MBP.home>
2015-06-19 14:47:10 -0400

    Issue #282
    Modified for csv file

--- app/models/URLGenerator.java
+++ app/models/URLGenerator.java
@@ -1,5 +1,6 @@
 package models;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -9,22 +10,38 @@ import java.net.URI;
 import java.net.URL;
 
 public class URLGenerator {
+	private static final String COMMA_DELIMITER = ",";
+	private static final String NEW_LINE_SEPERATOR = "\n";
+	
 	public static void generateURL() throws IOException {
 		FileInputStream in = null;
 		BufferedReader reader = null;
 		FileWriter writer = null;
+		String all = "";
 		try {
-			in = new FileInputStream("inputParams.txt");
+			in = new FileInputStream("URLFile/rogerLinks.csv");
 			reader = new BufferedReader(new InputStreamReader(in));
-			String campaignID = String.valueOf((int)(Math.random()*9000)+1000);
-			String contentURL = reader.readLine();
-			String productName = reader.readLine();
-			String productSlug = reader.readLine();
-			String output = "http://sprtup.io/" + campaignID + "?content=" + contentURL + 
-					"?product_name=" + productName + "?product_slug=" + productSlug;
-			output = convertToUrl(output).toString();
-			writer = new FileWriter("outputURL.txt");
-			writer.write(output);
+			String line = "";
+			all = reader.readLine();
+			while ((line = reader.readLine()) != null) {
+				String[] tokens = line.split(COMMA_DELIMITER);
+				if (tokens.length > 0) {
+					if (tokens.length < 5) {
+						String campaignID = String.valueOf((int)(Math.random()*9000)+1000);
+						String genURL = "http://sprtup.io/" + campaignID + 
+								"?content=" + tokens[0] + 
+								"?product_name=" + tokens[1] + 
+								"?product_slug=" + tokens[2];
+						genURL = convertToUrl(genURL).toString();
+						line = tokens[0] + COMMA_DELIMITER +
+								tokens[1] + COMMA_DELIMITER +
+								tokens[2] + COMMA_DELIMITER +
+								genURL + COMMA_DELIMITER +
+								campaignID;
+					}
+					all = all + NEW_LINE_SEPERATOR + line;
+				}
+			}
 		} catch(FileNotFoundException e) {
 			System.out.println("File named inputParams.txt cannot be founds");
 			e.printStackTrace();
@@ -34,6 +51,18 @@ public class URLGenerator {
 		} finally {
 			reader.close();
 			in.close();
+		}
+		try {
+			File file = new File ("URLFile/rogerLinks.csv");
+			if(file.delete()) {
+				writer = new FileWriter("URLFile/rogerLinks.csv");
+				writer.append(all);
+			}
+			else {
+				writer = new FileWriter("URLFile/rogerLinksMod.csv");
+				writer.append(all);
+			}
+		} finally {
 			writer.close();
 		}
 	}
